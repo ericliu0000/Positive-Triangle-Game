629a7f1cb80993bb914f90953ea08fd05b566f92
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-11 16:01:14 -0400

    [doesn't work] Vertexification work
    Added some edge detection stuff, not finished yet

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import itertools
+
 class Complex:
     def generate_vertices(n):
         return [i for i in range(n)]
@@ -61,23 +63,39 @@ class TriangleLadder:
 
 class StrangeDual:
     def generate_vertices(edges):
-        return [i for i in range(len(edges))]
+        return [i + 1 for i in range(len(edges))]
 
     def generate_edges(edges):
         ordered_edges = {}
 
         for count, edge in enumerate(edges):
-            ordered_edges[count] = edge
+            ordered_edges[count + 1] = edge
 
-        print(edge)
+        print(ordered_edges)
+
+        possibilities = itertools.combinations(range(1, len(edges) + 1), 2)
+
+        for pair in possibilities:
+            edge_pack = [ordered_edges[pair[0]][0], ordered_edges[pair[1]][0], ordered_edges[pair[0]][1], ordered_edges[pair[1]][1]]
+            print(edge_pack)
+            print(set(edge_pack))
+            print(list(set(edge_pack)) != edge_pack)
+            print("\n")
+            # Assuming there no self edges, 
+            # check for duplicates in edge pack
+            # if set(edge_pack) != edge_pack:
 
 
 if __name__ == "__main__":
-    test = StrangeDual()
 
     test_graph = Wheel.generate_edges(4)
 
-    print(test.generate_edges(test_graph))
-    )
+    # print(StrangeDual.generate_vertices(test_graph))
+    # print("\n\n\n")
+    print(StrangeDual.generate_edges(test_graph))
+    
 
 
+    x = itertools.combinations(range(1, 7), 2)
+    for i in x:
+        print(i)
