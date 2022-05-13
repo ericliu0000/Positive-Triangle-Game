db7be0fa270edf3e9467672642b6f844d5d0e55a
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-13 14:04:04 -0400

    vertex layout edit

--- vertexify.py
+++ vertexify.py
@@ -7,8 +7,9 @@ class Vertexify(Scene):
         self.camera.background_color = WHITE
         
         w = int(input())
-        test_edges = Wheel.generate_edges(w)
+        test_edges = TriangleLadder.generate_edges(w)
 
+        # graph layout is finnicky
         graph = Graph(StrangeDual.generate_vertices(test_edges),
                 StrangeDual.generate_edges(test_edges),
                 layout="kamada_kawai").scale(1.5)
