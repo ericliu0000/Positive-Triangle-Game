a14da5d75a74419c312946dbc0742c50f41a5530
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-13 15:58:34 -0400

    i put the oof in big oof

--- util.py
+++ util.py
@@ -61,7 +61,7 @@ class TriangleLadder:
         print(edges)
         return edges
 
-class StrangeDual:
+class Vertexify:
     def generate_vertices(edges):
         return [i + 1 for i in range(len(edges))]
 
@@ -77,11 +77,30 @@ class StrangeDual:
         possibilities = itertools.combinations(range(1, len(edges) + 1), 2)
 
         for pair in possibilities:
+            e1 = list(ordered_edges[pair[0]])
+            e2 = list(ordered_edges[pair[1]])
+
             edge_pack = [ordered_edges[pair[0]][0], ordered_edges[pair[1]][0], ordered_edges[pair[0]][1], ordered_edges[pair[1]][1]]
 
-            # check for duplicates in edge pack
             if len(edge_pack) != len(set(edge_pack)):
-                output.append((pair[0], pair[1]))
+                common = set(e1).intersection(e2).pop()
+                print(common)
+                print(e1, e2)
+                e1.remove(common)
+                e2.remove(common)
+                print(e1, e2, "\n")
+
+                # if (e1[0], e2[0])
+
+            # if any of the edges are the same
+            # if e1[0] == e2[0] or e1[0] == e2[1] or e1[1] == e2[0] or e1[1] == e2[1]:
+            #     output.append((pair[0], pair[1]))
+            # edge_pack = [ordered_edges[pair[0]][0], ordered_edges[pair[1]][0], ordered_edges[pair[0]][1], ordered_edges[pair[1]][1]]
+            
+
+            # # check for duplicates in edge pack
+            # if len(edge_pack) != len(set(edge_pack)):
+            #     output.append((pair[0], pair[1]))
 
         print(f"output: {output}")
 
@@ -95,10 +114,12 @@ if __name__ == "__main__":
 
     # print(StrangeDual.generate_vertices(test_graph))
     # print("\n\n\n")
-    print(StrangeDual.generate_edges(test_graph))
+    print(Vertexify.generate_edges(test_graph))
     
 
 
     # x = itertools.combinations(range(1, 7), 2)
     # for i in x:
     #     print(i)
+
+    # adjacency in the wheel graph: reject if there is no such edge between two edges that share a vertex but for which there is no edge between the non shared edges
--- vertexify.py
+++ vertexify.py
@@ -7,11 +7,11 @@ class Vertexify(Scene):
         self.camera.background_color = WHITE
         
         w = int(input())
-        test_edges = TriangleLadder.generate_edges(w)
+        test_edges = Wheel.generate_edges(w)
 
         # graph layout is finnicky
-        graph = Graph(StrangeDual.generate_vertices(test_edges),
-                StrangeDual.generate_edges(test_edges),
+        graph = Graph(Vertexify.generate_vertices(test_edges),
+                Vertexify.generate_edges(test_edges),
                 layout="kamada_kawai").scale(1.5)
         graph.set_color(BLACK)
 
