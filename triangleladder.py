629672d3b77333eb3df19a55744e6c2e51aad040
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-17 13:14:59 -0400

    add command line args

--- triangleladder.py
+++ triangleladder.py
@@ -1,13 +1,21 @@
+from multiprocessing.sharedctypes import Value
 from manim import *
 from util import *
+from sys import argv
 
 
 class Ladder(Scene):
     def construct(self):
         self.camera.background_color = WHITE
         
-        w = int(input())
+        try: 
+            w = int(argv[-1])
+        except ValueError as e:
+            w = 4
+            print("last argument specifies number of vertices")
 
+
+        # TODO: layout is ugly. please fix
         graph = Graph(TriangleLadder.generate_vertices(w),
                 TriangleLadder.generate_edges(w),
                 layout="spring").scale(1.5)
