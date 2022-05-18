3536d29632373e0080266896583bdb5600d36dcd
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-18 15:53:41 -0400

    cleanup

--- vertexify.py
+++ vertexify.py
@@ -16,7 +16,6 @@ class Vertexify(Scene):
 
         test_edges = wheel.WheelGraph().generate_edges(w)
 
-        # graph layout is finnicky
         graph = Graph(self.generate_vertices(test_edges),
                       self.generate_edges(test_edges),
                       layout="kamada_kawai").scale(1.5)
@@ -35,11 +34,8 @@ class Vertexify(Scene):
         for count, edge in enumerate(edges):
             ordered_edges[count + 1] = edge
 
-        print(ordered_edges)
-
-        possibilities = itertools.combinations(range(1, len(edges) + 1), 2)
-
-        for pair in possibilities:
+        # Iterate through every edge possibility
+        for pair in itertools.combinations(range(1, len(edges) + 1), 2):
             e1 = list(ordered_edges[pair[0]])
             e2 = list(ordered_edges[pair[1]])
 
@@ -48,35 +44,15 @@ class Vertexify(Scene):
 
             if len(edge_pack) != len(set(edge_pack)):
                 common = set(e1).intersection(e2).pop()
-                print(f"1: {common}")
-                print(f"2: {e1} {e2}")
 
                 # Remove the common vertex
                 e1.remove(common)
                 e2.remove(common)
 
-                print(f"3: {e1} {e2}")
-
                 # Get expected edge to complete face
                 missing = (e1[0], e2[0])
-                # missing2 = (e2[0], e1[0])
-
-                print(f"4: {missing in edges or missing[::-1] in edges}\n")
 
                 if missing in edges or missing[::-1] in edges:
                     output.append((pair[0], pair[1]))
-
-                # if (e1[0], e2[0])
-
-            # if any of the edges are the same
-            # if e1[0] == e2[0] or e1[0] == e2[1] or e1[1] == e2[0] or e1[1] == e2[1]:
-            #     output.append((pair[0], pair[1]))
-            # edge_pack = [ordered_edges[pair[0]][0], ordered_edges[pair[1]][0], ordered_edges[pair[0]][1], ordered_edges[pair[1]][1]]
-
-            # # check for duplicates in edge pack
-            # if len(edge_pack) != len(set(edge_pack)):
-            #     output.append((pair[0], pair[1]))
-
-        print(f"output: {output}")
 
         return output
