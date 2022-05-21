0c6e4c7c783d904d1239f0043bb20596f8bdafb0
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-20 23:11:25 -0400

    Intro scene prep, text automation
    Not a lot of stuff: add the method to bulk create text object from
    a multiline string

--- intro_cycle.py
+++ intro_cycle.py
@@ -1,4 +1,36 @@
 from manim import *
+from constants import *
+from graphs.complete import CompleteGraph
+import util
+
 
 class IntroCycle(Scene):
     def construct(self):
+        text = """This is 3-cycle graph.
+        It looks like a triangle.
+        #
+        In this game, the goal is to end
+        with one triangle with 0 or 2 (-) marks.
+        """
+
+        g = Graph(CompleteGraph().generate_vertices(3),
+                  CompleteGraph().generate_edges(3),
+                  layout="kamada_kawai").shift(UP)
+
+        text_objects = util.text_generator(text, DOWN)
+
+        print(text_objects)
+
+        self.play(Create(g))
+
+        self.play(Create(text_objects[0][0]))
+        self.play(Create(text_objects[0][1]))
+
+        self.wait(SHORT_DWELL_TIME)
+
+        self.play(ReplacementTransform(text_objects[0][0], text_objects[1][0]),
+                  ReplacementTransform(text_objects[0][1], text_objects[1][1]))
+        
+        self.wait(LONG_DWELL_TIME)
+
+
---
+++ util.py
@@ -0,0 +1,29 @@
+from manim import MarkupText
+from constants import *
+
+def text_generator(text, ref):
+    lines = text.split("\n")
+    print(lines)
+    groups = []
+
+    temp = []
+    for line in lines:
+        line = line.strip()
+
+        if line.startswith("#"):
+            groups.append(temp)
+            temp = []
+        else:
+            obj = MarkupText(line, color=TEXT_COLOR)
+
+            if len(temp) == 0:
+                obj.next_to(ref, DOWN, buff=TOP_TEXT_BUFFER)
+            else:
+                obj.next_to(temp[-1], DOWN, buff=BETWEEN_TEXT_BUFFER)
+
+            temp.append(obj)
+
+    groups.append(temp)
+
+    return groups
+
