773e4de67147474c83a3f1452d41e8b01668e780
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-05-22 11:08:25 -0400

    Complete play file start

---
+++ complete_play.py
@@ -0,0 +1,11 @@
+from manim import *
+from ..constants import *
+from ..graphs.complete import CompleteGraph
+import util
+
+
+class CompletePlay(Scene):
+    complete = CompleteGraph()
+
+    def construct(self):
+        pass
